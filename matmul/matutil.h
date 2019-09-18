0ac5539da0160a09043781ad9f0cb7100cd43633
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-09-18 10:13:38 +0200

    Renamed matmul to matutil, added addition and ReLu

---
+++ matmul/matutil.h
@@ -0,0 +1,16 @@
+#ifndef MATUTIL_H
+#define MATUTIL_H
+
+#include <stdio.h>
+
+void matutil_get_new_dimensions(int w1, int h1, int w2, int h2, int *wr, int *hr);
+
+int matutil_multiply(float *m1, int w1, int h1, float *m2, int w2, int h2, float *ret);
+
+int matutil_add(float *m1, int w1, int h1, float *m2, int w2, int h2, float *ret);
+
+void matutil_relu(float *m, int w, int h);
+
+void matutil_dump_matrix(float *m, int w, int h);
+
+#endif /* MATUTIL_H */
