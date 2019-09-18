2e331e55c604173a51ee1a37fd66e0c40c950260
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-09-18 14:27:17 +0200

    Major cleanup

---
+++ lib/native/matutil.c
@@ -0,0 +1,67 @@
+#include "matutil.h"
+
+int matutil_initialize(void){
+  return 0;
+}
+
+int matutil_teardown(void){
+  return 0;
+}
+
+void matutil_get_new_dimensions(int w1, int h1, int w2, int h2, int *wr,
+                               int *hr) {
+  *wr = w2;
+  *hr = h1;
+}
+
+int matutil_multiply(float *m1, int w1, int h1, float *m2, int w2, int h2,
+                    float *ret) {
+  // check dimensions
+  if (w1 != h2) {
+    fprintf(stderr, "Matrices have incompatible dimensions for multiplication %dx%d and %dx%d\n",
+            w1, h1, w2, h2);
+    return -1;
+  }
+  
+  int wr = w2, hr = h1;
+  for (int y = 0; y < hr; ++y) { // coordinates in ret
+    for (int x = 0; x < wr; ++x) {
+      ret[y * wr + x] = 0;
+      for (int i = 0, j = 0; i < w1; ++i, ++j) {
+        ret[y * wr + x] += m1[y * w1 + j] * m2[i * w2 + x];
+      }
+    }
+  }
+  return 0;
+}
+
+int matutil_add(float *m1, int w1, int h1, float *m2, int w2, int h2, float *ret){
+  if(w1 != w2 || w2 != h2){
+    fprintf(stderr, "Matrices have incompatible dimensions for addition %dx%d and %dx%d\n",
+            w1, h1, w2, h2);
+    return -1;
+  }
+
+  for(int i=0; i<h1; ++i){
+    for (int j=0; j < w1; ++j) {
+      int coord = i*w1+j;
+      ret[coord] = m1[coord] + m2[coord];
+    }
+  }
+  return 0;
+}
+
+void matutil_relu(float *m, int w, int h){
+  for(int i=0; i<w*h; i++)
+    if(m[i]<0)
+      m[i] = 0;
+}
+
+void matutil_dump_matrix(float *m, int w, int h) {
+  for (int i = 0; i < h; ++i) {
+    for (int j = 0; j < w; ++j) {
+      printf("%f, ", m[i * w + j]);
+    }
+    printf("\n");
+  }
+}
---
+++ mnist.py
@@ -0,0 +1,185 @@
+""" Test script for a pytorch MNIST CNN"""
+
+import argparse
+import os
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+
+class Net(nn.Module):
+    """ Defines the neural net for testing """
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4 * 4 * 50, 500, bias=True)
+        self.fc2 = nn.Linear(500, 10, bias=True)
+
+    def dense(self, x):
+        state = self.state_dict()
+        # breakpoint()
+        w1 = state['fc1.weight'].detach().numpy().T.astype(np.float32)
+        b1 = state['fc1.bias'].detach().numpy()
+        x = x.detach().numpy()
+        tmpb = np.matmul(x, w1) + b1
+        x = np.maximum(tmp, 0)
+
+        w2 = state['fc2.weight'].detach().numpy().T.astype(np.float32)
+        b2 = state['fc2.bias'].detach().numpy()
+        tmp = np.matmul(x, w2) + b2
+        x = np.maximum(tmp, 0)
+        return torch.Tensor(x)
+
+        # ReLu is just setting negative values to 0
+        # x = F.relu(self.fc1(x))
+        # return self.fc2(x)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4 * 4 * 50)
+        x = self.dense(x)  # move to C
+        return F.log_softmax(x, dim=1)
+        # return torch.zeros([x.shape[0], x.shape[1]])
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(
+                output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(
+                dim=1,
+                keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print(
+        '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+
+
+if __name__ == '__main__':
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size',
+                        type=int,
+                        default=128,
+                        metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--test-batch-size',
+                        type=int,
+                        default=1000,
+                        metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs',
+                        type=int,
+                        default=1,
+                        metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr',
+                        type=float,
+                        default=0.01,
+                        metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum',
+                        type=float,
+                        default=0.5,
+                        metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda',
+                        action='store_true',
+                        default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed',
+                        type=int,
+                        default=1,
+                        metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument(
+        '--log-interval',
+        type=int,
+        default=10,
+        metavar='N',
+        help='how many batches to wait before logging training status')
+
+    parser.add_argument('--save-model',
+                        action='store_true',
+                        default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(datasets.MNIST(
+        'data',
+        train=True,
+        download=True,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307, ), (0.3081, ))
+        ])),
+                                               batch_size=args.batch_size,
+                                               shuffle=True,
+                                               **kwargs)
+    test_loader = torch.utils.data.DataLoader(datasets.MNIST(
+        'data',
+        train=False,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307, ), (0.3081, ))
+        ])),
+                                              batch_size=args.test_batch_size,
+                                              shuffle=True,
+                                              **kwargs)
+
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(),
+                          lr=args.lr,
+                          momentum=args.momentum)
+
+    save_file = "models/mnist_cnn.pt"
+    if os.path.isfile(save_file):
+        print('loading from %s' % save_file)
+        model.load_state_dict(torch.load(save_file), strict=False)
+    else:
+        for epoch in range(1, args.epochs + 1):
+            train(args, model, device, train_loader, optimizer, epoch)
+            test(args, model, device, test_loader)
+
+        # if args.save_model:
+        torch.save(model.state_dict(), save_file)
