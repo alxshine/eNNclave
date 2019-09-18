ffaa92dc0ca494898b5b8adfaca54c8e9ea9ce8d
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-09-18 16:49:17 +0200

    Changed the signatures of matutil so it's rows*columns now

--- inc/matutil.h
+++ inc/matutil.h
@@ -7,14 +7,14 @@ int matutil_initialize();
 
 int matutil_teardown();
 
-void matutil_get_new_dimensions(int w1, int h1, int w2, int h2, int *wr, int *hr);
+void matutil_get_new_dimensions(int r1, int c1, int r2, int c2, int *rr, int *cr);
 
-int matutil_multiply(float *m1, int w1, int h1, float *m2, int w2, int h2, float *ret);
+int matutil_multiply(float *m1, int r1, int c1, float *m2, int r2, int c2, float *ret);
 
-int matutil_add(float *m1, int w1, int h1, float *m2, int w2, int h2, float *ret);
+int matutil_add(float *m1, int r1, int c1, float *m2, int r2, int c2, float *ret);
 
-void matutil_relu(float *m, int w, int h);
+void matutil_relu(float *m, int r, int c);
 
-void matutil_dump_matrix(float *m, int w, int h);
+void matutil_dump_matrix(float *m, int r, int c);
 
 #endif /* MATUTIL_H */
--- lib/native/matutil.c
+++ lib/native/matutil.c
@@ -1,66 +1,67 @@
 #include "matutil.h"
 
-int matutil_initialize(void){
-  return 0;
+int matutil_initialize(void) { return 0; }
+
+int matutil_teardown(void) { return 0; }
+
+void matutil_get_new_dimensions(int r1, int c1, int r2, int c2, int *rr,
+                                int *cr) {
+  *rr = r1;
+  *cr = c2;
 }
 
-int matutil_teardown(void){
-  return 0;
-}
-
-void matutil_get_new_dimensions(int w1, int h1, int w2, int h2, int *wr,
-                               int *hr) {
-  *wr = w2;
-  *hr = h1;
-}
-
-int matutil_multiply(float *m1, int w1, int h1, float *m2, int w2, int h2,
-                    float *ret) {
+int matutil_multiply(float *m1, int r1, int c1, float *m2, int r2, int c2,
+                     float *ret) {
   // check dimensions
-  if (w1 != h2) {
-    fprintf(stderr, "Matrices have incompatible dimensions for multiplication %dx%d and %dx%d\n",
-            w1, h1, w2, h2);
+  if (r1 != c2) {
+    fprintf(stderr,
+            "Matrices have incompatible dimensions for multiplication %dx%d "
+            "and %dx%d\n",
+            r1, c1, r2, c2);
     return -1;
   }
-  
-  int wr = w2, hr = h1;
-  for (int y = 0; y < hr; ++y) { // coordinates in ret
-    for (int x = 0; x < wr; ++x) {
-      ret[y * wr + x] = 0;
-      for (int i = 0, j = 0; i < w1; ++i, ++j) {
-        ret[y * wr + x] += m1[y * w1 + j] * m2[i * w2 + x];
+
+  int rr = r1, cr = c2;
+  for (int y = 0; y < rr; ++y) { // coordinates in ret
+    for (int x = 0; x < cr; ++x) {
+      ret[y * cr + x] = 0;
+      for (int i = 0, j = 0; i < r1; ++i, ++j) {
+        ret[y * cr + x] += m1[y * c1 + j] * m2[i * c2 + x];
       }
     }
   }
   return 0;
 }
 
-int matutil_add(float *m1, int w1, int h1, float *m2, int w2, int h2, float *ret){
-  if(w1 != w2 || h1 != h2){
-    fprintf(stderr, "Matrices have incompatible dimensions for addition %dx%d and %dx%d\n",
-            w1, h1, w2, h2);
+int matutil_add(float *m1, int r1, int c1, float *m2, int r2, int c2,
+                float *ret) {
+  if (r1 != r2 || c1 != c2){
+    fprintf(
+        stderr,
+        "Matrices have incompatible dimensions for addition %dx%d and %dx%d\n",
+        r1, c1, r2, c2);
     return -1;
   }
 
-  for(int i=0; i<h1; ++i){
-    for (int j=0; j < w1; ++j) {
-      int coord = i*w1+j;
+  for (int i = 0; i < r1; ++i) {
+    for (int j = 0; j < c1; ++j) {
+      int coord = i * c1 + j;
       ret[coord] = m1[coord] + m2[coord];
     }
   }
   return 0;
 }
 
-void matutil_relu(float *m, int w, int h){
-  for(int i=0; i<w*h; i++)
-    if(m[i]<0)
+void matutil_relu(float *m, int r, int c) {
+  for (int i = 0; i < r*c; i++)
+    if (m[i] < 0)
       m[i] = 0;
 }
 
-void matutil_dump_matrix(float *m, int w, int h) {
-  for (int i = 0; i < h; ++i) {
-    for (int j = 0; j < w; ++j) {
-      printf("%f, ", m[i * w + j]);
+void matutil_dump_matrix(float *m, int r, int c) {
+  for (int i = 0; i < r; ++i) {
+    for (int j = 0; j < c; ++j) {
+      printf("%f, ", m[i * c + j]);
     }
     printf("\n");
   }
