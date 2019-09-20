1dd97d941a54a4bdaa31ee7507bbf03b6c8c5314
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-09-20 12:02:05 +0200

    Fixed accuracy output

--- mnist.py
+++ mnist.py
@@ -83,28 +83,14 @@ class Net(nn.Module):
 
         labels = self.full_dense(x)
 
-        if not np.array_equal(ret_test.argmax(
+        if np.array_equal(ret_test.argmax(
                 dim=1), labels):  # check my dense code for correct labels
+            print("Labels are identical with reference torch output")
+        else:
             print(
                 "ERROR: Labels are NOT identical with reference torch output")
 
         return labels
-
-
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-
 
 def test(args, model, device, test_loader):
     model.eval()
@@ -120,7 +106,7 @@ def test(args, model, device, test_loader):
 
     print(
         '\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
+            correct, len(test_loader.dataset),
             100. * correct / len(test_loader.dataset)))
 
 
