133da9ed8d2d5be5780131e804f7aa48360cc6e5
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-09-25 18:35:28 +0200

    Did some testing with how Keras executes functions

--- python/keras_mnist.py
+++ python/keras_mnist.py
@@ -1,38 +1,80 @@
-from keras.layers import Input, Dense, Conv2D, Flatten
-from keras.models import Model, Sequential
-from keras.utils import plot_model
-from keras.utils.layer_utils import print_summary
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import keras_enclave
 
-# # This returns a tensor
-# inputs = Input(shape=(28,28,1), name='input')
+import os
 
-# # a layer instance is callable on a tensor, and returns a tensor
-# conv1 = Conv2D(20, 5, name='conv1')(inputs)
-# conv2 = Conv2D(50, 5, name='conv2')(conv1)
-# flatten = Flatten(name='flatten')(conv2)
-# fc1 = Dense(500, activation='relu',name='fc1')(flatten)
-# fc2 = Dense(10, activation='relu',name='fc2')(fc1)
-# predictions = Dense(10, activation='softmax',name='softmax')(fc2)
+batch_size = 128
+num_classes = 10
+epochs = 1
 
-# # This creates a model that includes
-# # the Input layer and three Dense layers
-# model = Model(inputs=inputs, outputs=predictions, name='Sequential API')
-# model.compile(optimizer='rmsprop',
-#               loss='categorical_crossentropy',
-#               metrics=['accuracy'])
-# model.summary()
+# input image dimensions
+img_rows, img_cols = 28, 28
 
-dense_part = Sequential([
-    Dense(500, activation='relu',name='fc1'),
-    Dense(10, activation='relu',name='fc2'),
-    Dense(10, activation='softmax',name='softmax')],
-    name='SGX')
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-model = Sequential([
-    Conv2D(20,5, name='conv1', input_shape=(28,28,1)),
-    Conv2D(50,5, name='conv2'),
-    Flatten(name='flatten'),
-    dense_part
-], name='Model API')
-# model.summary()
-# plot_model(model, to_file='mnist_cnn.png', expand_nested=True)
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# load if possible
+model_name = "models/keras_mnist_cnn.h5"
+if os.path.exists(model_name):
+    print("Model found, loading from %s" % model_name)
+    model = load_model(model_name)
+else:
+    print("No model found, creating")
+    dense_part = Enclave([
+        Dense(500, activation='relu',name='fc1'),
+        Dense(10, activation='relu',name='fc2'),
+        Dense(10, activation='softmax',name='softmax')],
+        name='SGX')
+
+    model = Sequential([
+        Conv2D(20,5, name='conv1', input_shape=(28,28,1)),
+        Conv2D(50,5, name='conv2'),
+        Flatten(name='flatten'),
+        dense_part
+    ], name='Model API')
+
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+    
+    print("Storing newly created model in %s" % model_name)
+    model.save(model_name)
+
+# score = model.evaluate(x_test, y_test, verbose=0)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
+breakpoint()
+model.predict(x_test[0:1])
