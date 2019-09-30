801d6219842de54e68e34732bef665191af0d2ba
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-09-30 13:27:31 +0200

    Added testing code for enclave layer

--- python/keras_enclave.py
+++ python/keras_enclave.py
@@ -7,12 +7,13 @@ import numpy as np
 
 
 class EnclaveLayer(Layer):
-    def wrap_matutil(self, x):
-        x = x.numpy()
-        label = pymatutil.dense(x.astype(np.float32).tobytes(),
-                                x.shape[0], x.shape[1])
-        ret = np.zeros(self.num_outputs, dtype=np.float32)
-        ret[label] = 1
+    def wrap_matutil(self, xs):
+        xs = xs.numpy()
+        ret = np.zeros_like(xs)
+        for i, x in enumerate(xs):
+            label = pymatutil.dense(x.astype(np.float32).tobytes(),
+                                    1, x.shape[0])
+            ret[i, label] = 1
         return ret
 
     def __init__(self, num_outputs):
