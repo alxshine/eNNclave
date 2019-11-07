b86246e77bad9ece9524d14406e2d4a77d97f97b
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-11-07 07:53:09 +0100

    More planning, reworked vgg_lfw structure

--- vgg_lfw.py
+++ vgg_lfw.py
@@ -32,102 +32,28 @@ IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
 
 model_file = 'models/vgg_lfw.h5'
 
+# ------------------ MODEL DEFINITION --------------------
+
 VGG16_MODEL = VGG16(input_shape=IMG_SHAPE,
                     include_top=False, weights='imagenet')
 VGG16_MODEL.trainable = False
 
-hidden_neurons = 4096
-steps_per_epoch = 40
-num_epochs = 100
+hidden_neurons = 512
+steps_per_epoch = 4
+num_epochs = 1000
 
-# ----------- MODEL DEFINITIONS -----------
-
-# "baseline" variant, reaches 61%
-# enclave = Enclave([
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(NUM_CLASSES, activation='softmax')
-# ])
-# dense = tf.keras.Sequential([
-#     layers.GlobalAveragePooling2D(),
-#     enclave
-# ])
-
-# "shorter" variant, with half the training epochs
-# num_epochs = 50
-# hidden_neurons = 512
-# enclave = Enclave([
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(NUM_CLASSES, activation='softmax')
-# ])
-# dense = tf.keras.Sequential([
-#     layers.GlobalAveragePooling2D(),
-#     enclave
-# ])
-
-# "longer" variant, with double the training epochs, reaches 62%
-# num_epochs = 200
-# enclave = Enclave([
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(NUM_CLASSES, activation='softmax')
-# ])
-# dense = tf.keras.Sequential([
-#     layers.GlobalAveragePooling2D(),
-#     enclave
-# ])
-
-# "flattened" variant, with less input reduction, reaches 63%
-num_epochs = 50
 enclave = Enclave([
     layers.Dense(hidden_neurons, activation='relu'),
     layers.Dense(hidden_neurons, activation='relu'),
     layers.Dense(NUM_CLASSES, activation='softmax')
 ])
 dense = tf.keras.Sequential([
-    layers.Flatten(),
+    layers.GlobalAveragePooling2D(),
     enclave
 ])
 
-# "larger" variant, with more dense neurons, reaches 62%
-# hidden_neurons = 8192
-# enclave = Enclave([
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(NUM_CLASSES, activation='softmax')
-# ])
-# dense = tf.keras.Sequential([
-#     layers.GlobalAveragePooling2D(),
-#     enclave
-# ])
+# -------------- END MODEL DEFINITION --------------------
 
-# large variant on flattened conv output, reaches 58%
-# hidden_neurons = 8192
-# enclave = Enclave([
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(NUM_CLASSES, activation='softmax')
-# ])
-# dense = tf.keras.Sequential([
-#     layers.Flatten(),
-#     enclave
-# ])
-
-# large variant on flattened conv output, longer training, reaches 65%
-# hidden_neurons = 8192
-# num_epochs = 200
-# enclave = Enclave([
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(hidden_neurons, activation='relu'),
-#     layers.Dense(NUM_CLASSES, activation='softmax')
-# ])
-# dense = tf.keras.Sequential([
-#     tf.keras.layers.Flatten(),
-#     enclave
-# ])
-
-# ----------- COMMON AGAIN -----------
 print('Hypeparameters:')
 print('num_epochs: {}'.format(num_epochs))
 print('hidden_neurons: {}'.format(hidden_neurons))
