65b0cd8b6ef5c07a9dffded607b8ce138e44bd6e
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-11-12 15:28:26 +0100

    Added files required to clean build the project

---
+++ clean_project.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+make clean
+cp dummy_forward.cpp lib/native/forward.cpp
+cp dummy_state.cpp lib/native/state.cpp
---
+++ dummy_forward.cpp
@@ -0,0 +1,6 @@
+#include "forward.hpp"
+
+int forward(float *m, int, int r, int c, int *label){
+
+  return 0;
+}
