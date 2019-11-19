75236228376c06c41ad4af7c6f78cdcdbd03a0cb
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-11-19 16:11:18 +0100

    Fixed problems in the compare_outputs for MIT67

--- mit67_compare_outputs.py
+++ mit67_compare_outputs.py
@@ -12,21 +12,22 @@ from os.path import join
 import json
 import sys
 
-tf.compat.v1.set_random_seed(1337)
-np.random.seed(1337)
+tf.compat.v1.enable_eager_execution()
 
-#TODO: use argparse
+if len(sys.argv) < 3:
+    print("Usage: {} tf_model enclave_model".format(sys.argv[0]))
 
-model_file_1 = sys.argv[1]
-model_file_2 = sys.argv[2]
-num_images = 100
+NUM_IMAGES = 10
 
-model_1 = load_model(model_file_1, custom_objects={"EnclaveLayer": EnclaveLayer})
-model_2 = load_model(model_file_2, custom_objects={"EnclaveLayer": EnclaveLayer})
+tf_model_file = sys.argv[1]
+tf_model = load_model(tf_model_file)
+enclave_model_file = sys.argv[2]
+enclave_model = load_model(enclave_model_file, custom_objects={'EnclaveLayer': EnclaveLayer})
 
 data_dir = 'data/mit67'
 test_file = 'TestImages.txt'
 
+print("Taking {} images from MIT67 test_set".format(NUM_IMAGES))
 # build label mapping from directories
 with open(join(data_dir, "class_labels.json"), 'r') as f:
     labels = json.load(f)
@@ -35,23 +36,30 @@ with open(join(data_dir, "class_labels.json"), 'r') as f:
 with open(join(data_dir, test_file), 'r') as f:
     test_images = [join(data_dir, l.strip()) for l in f.readlines()]
 # reduce dataset size
-test_images = np.random.choice(test_images, num_images)
+test_images = np.random.choice(test_images, NUM_IMAGES)
 test_labels = [labels[s.split('/')[-2]] for s in test_images]
+processed_images = np.zeros((len(test_images), 224, 224, 3))
+for i, image in enumerate(test_images):
+    image = tf.io.read_file(image)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.cast(image, tf.float32)
+    image = (image/127.5) - 1
+    image = tf.image.resize(image, (224, 224))
+    processed_images[i] = image
 
-test_ds = utils.generate_dataset(test_images, test_labels, shuffle=False, repeat=False)
+test_images = processed_images
 
 # predict dataset
-predictions_1 = model_1.predict(test_ds)
-# TODO: deal with models returning the label directly
-predictions_1 = np.argmax(predictions_1, axis=1)
+print("Predicting with TF model")
+tf_predictions = tf_model.predict(test_images)
+tf_labels = np.argmax(tf_predictions, axis=1)
 
-accuracy_1 = np.equal(predictions_1, test_labels).sum()/len(test_labels)
-print("Model 1 accuracy: {}".format(accuracy_1))
+tf_accuracy = np.equal(tf_labels, test_labels).sum()/len(test_labels)
+print("TF model accuracy: {}".format(tf_accuracy))
 
-predictions_2 = model_2.predict(test_ds)
+print("Predicting with Enclave model")
+enclave_predictions = enclave_model.predict(test_images)
+enclave_labels = np.argmax(enclave_predictions, axis=1)
 
-accuracy_2 = np.equal(predictions_2, test_labels).sum()/len(test_labels)
-print("Model 2 accuracy: {}".format(accuracy_2))
-
-same_labels = np.equals(predictions_1, predictions_2)
+same_labels = np.equal(tf_labels, enclave_labels)
 print("{} of {} labels are equal".format(same_labels.sum(), len(same_labels)))
