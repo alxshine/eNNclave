141cd7a3476760900c05524f1522497eb55d84f6
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-12-02 17:13:23 +0100

    Removed some unnecessary stuff

--- lib/sgx/App/wrapper.h
+++ lib/sgx/App/wrapper.h
@@ -1,35 +1,3 @@
-/*
- * Copyright (C) 2011-2019 Intel Corporation. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in
- *     the documentation and/or other materials provided with the
- *     distribution.
- *   * Neither the name of Intel Corporation nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-
 #ifndef _APP_H_
 #define _APP_H_
 
@@ -59,25 +27,6 @@ extern sgx_enclave_id_t global_eid;    /* global enclave id */
 #if defined(__cplusplus)
 extern "C" {
 #endif
-
-  void edger8r_array_attributes(void);
-  void edger8r_type_attributes(void);
-  void edger8r_pointer_attributes(void);
-  void edger8r_function_attributes(void);
-  
-  void ecall_libc_functions(void);
-  void ecall_libcxx_functions(void);
-  void ecall_thread_functions(void);
-
-  /* int matutil_initialize(); */
-  /* int matutil_teardown(); */
-  /* void matutil_get_new_dimensions(int r1, int c1, int r2, int c2, int *rr, */
-  /* 				  int *cr); */
-  /* int matutil_multiply(float *m1, int r1, int c1, float *m2, int r2, int c2, float *ret); */
-  /* int matutil_add(float *m1, int r1, int c1, float *m2, int r2, int c2, float *ret); */
-  /* void matutil_relu(float *m, int r, int c); */
-  /* int matutil_forward(float *m, int r, int c, int *label); */
-  /* void matutil_dump_matrix(float *m, int r, int c); */
 
   void test();
   
