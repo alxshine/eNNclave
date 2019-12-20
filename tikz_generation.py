99f4fde31dcbfa5c76ae21fb07a64cd5d12c0df7
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-12-20 11:56:24 +0100

    Added drawing of y axis, added rough stuff for time rectangles

--- tikz_generation.py
+++ tikz_generation.py
@@ -4,19 +4,45 @@ from utils import get_all_layers
 from enclave_layer import EnclaveLayer
 from enclave_model import Enclave
 
-import sys
+import numpy as np
 
-def get_tikz(model):
+import sys
+import json
+
+def _texify_number(num):
+    "uses a-j instead of 0-9 to work with tex"
+    ret = ''
+    num_digits = int(np.ceil(np.log10(num)))
+
+    # TODO: ugly hacks for edge cases that I don't want to deal with right now
+    if num_digits == 0:
+        return 'a'
+    if num == 10:
+        return 'ba'
+    
+    for i in range(num_digits):
+        int_val = ord('a') + (num % 10)
+        ret += chr(int_val)
+        num //= 10
+    return ret[::-1]
+
+def net_summary(model):
     start_x = 0
     width = 1.8
     height = 0.4
     node_distance = 0.5
     space_between = node_distance - height
 
-    graph_x_ticks = '\\newcommand{\\graphxticks}{'
-    layers = get_all_layers(model)
     ret = ''
-    ret += '\\newcommand{\\netsummary}[1]{\n'
+    ret += '\\newcommand{\\startx}{%f}\n' % (start_x)
+    ret += '\\newcommand{\\nodedistance}{%f}\n' % (node_distance)
+    ret += '\\newcommand{\\spacebetween}{%f}\n' % (space_between)
+    ret += '\\newcommand{\\layerheight}{%f}\n' % (height)
+    
+    x_ticks = '\\newcommand{\\xticks}{'
+    
+    layers = get_all_layers(model)
+    ret += '\n\\newcommand{\\netsummary}[1]{\n'
     for i,l in enumerate(layers):
         cleaned_name = l.name.replace('_','\_')
         current_x = start_x + node_distance*i
@@ -26,16 +52,41 @@ def get_tikz(model):
 
         if i > 0:
             if i > 1:
-                graph_x_ticks += ','
-            graph_x_ticks += '%f' % (current_x - space_between/2)
-        
+                x_ticks += ','
+            x_ticks += '%f' % (current_x - space_between/2)
+
+    x_ticks += '}\n'
     ret += '}\n'
 
-    graph_x_ticks += '}\n'
-    ret += graph_x_ticks
-
+    ret += x_ticks
     ret += '\\newcommand{\\netwidth}{%f}\n' % (current_x + height)
 
+    return ret
+
+def time_rectangles(time_dict):
+    y_max = 5
+    scale_bottom = -2.5 #value of -2.5 shall at 0
+    scale_top = 4.7 #value of 4.7 shall be at y_max
+    y_ticks = np.concatenate([np.arange(0.1, 1, 0.1), np.arange(1, 10, 1), np.arange(10, 110, 10)])
+
+    ret = ''
+    ret += '\\newcommand{\\ymax}{%f}\n' % y_max
+    ret += '\\newcommand{\\yticks}{'
+    for i,y in enumerate(y_ticks):
+        coordinate = (np.log(y)-scale_bottom)/(scale_top-scale_bottom)
+        
+        if i > 0:
+            ret += ','
+        ret += '%f' % (coordinate*y_max)
+    ret += '}\n'
+
+    for split in time_dict:
+        split = int(split)
+        x_coordinate = '\\netwidth-\\layerheight-%d*\\nodedistance-\\spacebetween/2' % (split-1)
+        node = '\\node[anchor=south, draw] at (%s, 0) {};' % x_coordinate
+
+        ret += '\\newcommand{\\split%s}{%s}\n' % (_texify_number(split), node)
+        
     return ret
 
 if __name__ == "__main__":
@@ -47,5 +98,9 @@ if __name__ == "__main__":
     model = load_model(model_file, custom_objects={
                        "Enclave": Enclave, "EnclaveLayer": EnclaveLayer})
 
-    tikz = get_tikz(model)
+    tikz = net_summary(model)
     print(tikz)
+
+    with open('timing_logs/mit67_times.json', 'r') as f:
+        time_dict = json.load(f)
+    print(time_rectangles(time_dict))
