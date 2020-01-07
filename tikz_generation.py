3c05f1438b813eaf1920bdb2696abe751b72e5b6
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-01-07 17:09:41 +0100

    Worked on graphic
    Split the columns, so comparison makes more sense on the log scale.
    Also added colors. They're ugly, but that's easily remedied.
    The y axis now has labels and is more readable

--- tikz_generation.py
+++ tikz_generation.py
@@ -27,11 +27,28 @@ def _texify_number(num):
         num //= 10
     return ret[::-1]
 
-def _calc_log_coord(lin_coord):
-    scale_bottom = -2.5 #value of -2.5 shall at 0
-    scale_top = 4.7 #value of 4.7 shall be at y_max
+def _build_log_scale(smallest_exponent, largest_exponent):
+    num_scales = largest_exponent - smallest_exponent + 1
+    y_ticks = np.empty(num_scales*9+1)
+    y_labels = []
+    for i, exp in enumerate(range(smallest_exponent, largest_exponent+1)):
+        base = 10**exp
+        y_labels.append((base, "10^{%d}" % exp))
+        for j in range(9):
+            y_ticks[i*9+j] = (j+1)*base
 
-    return (np.log(lin_coord)-scale_bottom)/(scale_top-scale_bottom)
+    y_ticks[-1] = 10*base
+    y_labels.append((10*base, "10^{%d}" % (exp + 1)))
+
+    return y_ticks, y_labels
+
+def _calc_log_coord(lin_coord):
+    scale_bottom = -3.2 #y value to be at tikz 0
+    scale_top = 2.2 #y value to be at y_max
+
+    scale_width = scale_top - scale_bottom
+    log_coord = np.log10(lin_coord)
+    return (log_coord-scale_bottom)/scale_width
 
 def net_summary(model):
     start_x = 0
@@ -71,45 +88,70 @@ def net_summary(model):
     return ret
 
 def time_rectangles(times):
-    y_max = 5
-    y_ticks = np.concatenate([np.arange(0.1, 1, 0.1), np.arange(1, 10, 1), np.arange(10, 110, 10)])
-    rectangle_width = 0.2
+    y_max = 7
+    y_ticks, y_labels = _build_log_scale(-3, 1)
+    rectangle_width = 0.15
 
     ret = ''
     ret += '\\newcommand{\\ymax}{%f}\n' % y_max
     ret += '\\newcommand{\\yticks}{'
-    for i,y in enumerate(y_ticks):
-        coordinate = _calc_log_coord(y)
-        
-        if i > 0:
-            ret += ','
-        ret += '%f' % (coordinate*y_max)
+    with np.errstate(all='raise'):
+        try:
+            for i,y in enumerate(y_ticks):
+                coordinate = _calc_log_coord(y)
+            
+                if i > 0:
+                    ret += ','
+                ret += '%f' % (coordinate*y_max)
+        except FloatingPointError as e:
+            print("ERROR: %s" % e, file=sys.stderr)
+            print("y: %f" % y, file=sys.stderr)
+
     ret += '}\n'
 
+    # generate time rectangles
     for i, row in times.iterrows():
         gpu_time = row['gpu_time']
         enclave_time = row['combined_enclave_time']
         native_time = row['native_time']
         split = int(row['layers_in_enclave'])
         
-        left_coordinate = '\\netwidth-\\layerheight-%d*\\nodedistance-\\spacebetween/2-%f' % (split-1, rectangle_width/2)
-        right_coordinate = left_coordinate + ("+%f" % rectangle_width)
+        left_0 = '\\netwidth-\\layerheight-%d*\\nodedistance-\\spacebetween/2-%f' % (split-1, rectangle_width*3/2)
+        right_0 = left_0 + ("+%f" % rectangle_width)
+        right_1 = left_0 + ("+%f" % (2*rectangle_width))
+        right_2 = left_0 + ("+%f" % (3*rectangle_width))
         
-        gpu_north = _calc_log_coord(gpu_time)*y_max
-        native_north = _calc_log_coord(gpu_time+native_time)*y_max
-        enclave_north = _calc_log_coord(gpu_time+enclave_time)*y_max
+        with np.errstate(all='raise'):
+            try:
+                gpu_north = _calc_log_coord(gpu_time)*y_max
+                native_north = _calc_log_coord(native_time)*y_max
+                enclave_north = _calc_log_coord(enclave_time)*y_max
+            except FloatingPointError as e:
+                print("ERROR: %s" % e, file=sys.stderr)
+                print("GPU time: %f, native time: %f, enclave time: %f" % (gpu_time, native_time, enclave_time), file=sys.stderr)
         
-        node = '\\draw (%s, 0) rectangle (%s, %f);\n' % (left_coordinate, right_coordinate, gpu_north)
-        node += '\\draw[pattern=north east lines] (%s, %f) rectangle (%s, %f);\n' % (left_coordinate, gpu_north, right_coordinate, native_north)
-        node += '\\draw[pattern=north west lines] (%s, %f) rectangle (%s, %f);\n' % (left_coordinate, native_north, right_coordinate, enclave_north)
+        node = '\\draw[fill=color1] (%s, 0) rectangle (%s, %f);\n' % (left_0, right_0, gpu_north)
+        node += '\\draw[fill=color2] (%s, 0) rectangle (%s, %f);\n' % (right_0, right_1, native_north)
+        node += '\\draw[fill=color3] (%s, 0) rectangle (%s, %f);\n' % (right_1, right_2, enclave_north)
 
         ret += '\\newcommand{\\split%s}{%s}\n' % (_texify_number(split), node)
+
+    # generate command for y labels
+    label_command = "\\newcommand{\\ylabels}[1]{\n"
+    for lin_y, label_text in y_labels:
+        log_y = _calc_log_coord(lin_y)
+
+        current_label = "\\draw (#1, %f) node {\\tiny $%s$};\n" % (log_y * y_max, label_text)
+        label_command += current_label
+    label_command += "}\n"
+
+    ret += label_command
         
     return ret
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: {} model_file".format(sys.argv[0]))
+    if len(sys.argv) < 3:
+        print("Usage: {} model_file time_file".format(sys.argv[0]))
         sys.exit(1)
     
     model_file = sys.argv[1]
@@ -119,6 +161,6 @@ if __name__ == "__main__":
     tikz = net_summary(model)
     print(tikz)
 
-    time_file = 'timing_logs/mit67_times.csv'
+    time_file = sys.argv[2]
     times = pd.read_csv(time_file)
     print(time_rectangles(times))
