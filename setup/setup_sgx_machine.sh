e0b6d5ae7016bf744e7fea644baeef935e264da4
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-02-06 20:40:36 +0100

    Fixed bug in setup script, updated slides

--- setup/setup_sgx_machine.sh
+++ setup/setup_sgx_machine.sh
@@ -9,12 +9,7 @@ sudo apt-get install linux-headers-$(uname -r)
 git clone git@github.com:intel/linux-sgx-driver.git
 cd linux-sgx-driver/
 make
-ll
-sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"
-sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"
-sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"
-sudo /sbin/depmod
-sudo /sbin/modprobe isgx
+sh ./install.sh
 
 # build SDK
 cd ~/Tools/
