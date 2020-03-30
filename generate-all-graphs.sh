110588a23f092904171bd7517192242816ae51e7
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-03-30 17:38:18 +0200

    Added script to generate all macros

---
+++ generate-all-graphs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python tikz_generation.py --model models/mit.h5 --time timing_logs/mit_times_cpu.csv timing_logs/mit_times_gpu.csv --ymin -3 --ymax 3 > mit.tex
+python tikz_generation.py --model models/imdb.h5 --time timing_logs/imdb_times_cpu.csv timing_logs/imdb_times_gpu.csv --ymin -3 --ymax 1 > imdb.tex
+python tikz_generation.py --model models/mnist.h5 --time timing_logs/mnist_times_cpu.csv timing_logs/mnist_times_gpu.csv --ymin -4 --ymax 1 > mnist.tex
+python tikz_generation.py --model models/rotten_tomatoes.h5 --time timing_logs/rotten_times_cpu.csv timing_logs/rotten_times_gpu.csv --ymin -3 --ymax 1 > rotten.tex
