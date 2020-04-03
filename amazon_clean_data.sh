fff85a5e8f54a8b9d500d7f8b38b8de2e27f6b7f
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-04-03 16:38:19 +0200

    Fixed loading for amazon dataset

--- amazon_clean_data.sh
+++ amazon_clean_data.sh
@@ -2,14 +2,12 @@
 #get data from here: https://www.kaggle.com/bittlingmayer/amazonreviews
 
 train_file='data/amazon/train.ft.txt'
+train_dest_file='data/amazon/train_cleaned.txt'
 test_file='data/amazon/test.ft.txt'
+test_dest_file='data/amazon/test_cleaned.txt'
 
 # train file
-echo -e "Sentiment\tPhrase" | cat $train_file > /tmp/train.ft.txt && mv /tmp/train.ft.txt $train_file
-sed -i 's/^__label__1 /1\t/' data/amazon/train.ft.txt
-sed -i 's/^__label__2 /2\t/' data/amazon/train.ft.txt
+sed -e '1s;^;Sentiment\tPhrase\r\n;' -e 's/__label__1 /0\t/' -e 's/__label__2 /1\t/' $train_file > $train_dest_file
 
 # test file
-echo -e "Sentiment\tPhrase" | cat $test_file > /tmp/test.ft.txt && mv /tmp/test.ft.txt $test_file
-sed -i 's/^__label__1 /1\t/' data/amazon/test.ft.txt
-sed -i 's/^__label__2 /2\t/' data/amazon/test.ft.txt
+sed -e '1s;^;Sentiment\tPhrase\r\n;' -e 's/__label__1 /0\t/' -e 's/__label__2 /1\t/' $test_file > $test_dest_file
