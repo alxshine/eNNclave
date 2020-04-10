b4379cee87f823128bccf1b85131570ad52622b6
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-04-10 08:52:10 +0200

    61%, 55%; added MAE to eval script

--- amazon_eval.py
+++ amazon_eval.py
@@ -13,11 +13,16 @@ print("Generating true training accuracy")
 train_predictions = model.predict(x_train, verbose = 0)
 train_cleaned_predictions = train_predictions.flatten().round()
 train_acc = np.mean(train_cleaned_predictions == y_train)
+train_mae = np.mean(np.abs(train_cleaned_predictions - y_train))
+
+print(f'True training accuracy: {train_acc*100:.4}')
+print(f'Training MAE: {train_mae:.4}')
 
 print("Generating true test accuracy")
 test_predictions = model.predict(x_test, verbose = 0)
 test_cleaned_predictions = test_predictions.flatten().round()
 test_acc = np.mean(test_cleaned_predictions == y_test)
+test_mae = np.mean(np.abs(test_cleaned_predictions - y_test))
 
-print(f'True training accuracy: {train_acc*100:.4}')
-print(f'True validation accuracy: {test_acc*100:.4}')
+print(f'True test accuracy: {test_acc*100:.4}')
+print(f'Test MAE: {test_mae:.4}')
