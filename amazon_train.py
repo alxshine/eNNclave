3b8b3f4073b1e2c2a1998c4d69c06b843bd4b028
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-04-10 12:03:24 +0200

    Added variant for large dataset

--- amazon_train.py
+++ amazon_train.py
@@ -14,7 +14,6 @@ import plotille
 SEED = 1337
 
 DATA_DIR = 'datasets/amazon'
-PICKLE_FILE = 'reduced.pkl'
 MODEL_FILE = 'models/amazon.h5'
 
 NUM_SAMPLES = 2000000
@@ -25,40 +24,77 @@ TOKENIZER_CONFIG_FILE = 'data/amazon_tokenizer_config.json'
 DROPOUT_RATE = 0.3
 HIDDEN_NEURONS = 600
 EPOCHS = 200 # this is where we start to overfit
+LARGE = True
 
-try:
-    # load numpy matrices
-    print(f"Trying to load training data from {DATA_DIR}")
+if LARGE:
+    try:
+        # load numpy matrices
+        print(f"Trying to load large training data from {DATA_DIR}")
 
-    x_train = np.load(join(DATA_DIR, 'x_train.npy'))
-    y_train = np.load(join(DATA_DIR, 'y_train.npy'))
-    x_test = np.load(join(DATA_DIR, 'x_test.npy'))
-    y_test = np.load(join(DATA_DIR, 'y_test.npy'))
-except IOError:
-    print("Not found, generating...")
-    data = pd.read_pickle(os.path.join(DATA_DIR, PICKLE_FILE))
-    data = data.sample(frac=1, replace=False, random_state = SEED)
+        x_train = np.load(join(DATA_DIR, 'x_train_large.npy'))
+        y_train = np.load(join(DATA_DIR, 'y_train_large.npy'))
+        x_test = np.load(join(DATA_DIR, 'x_test_large.npy'))
+        y_test = np.load(join(DATA_DIR, 'y_test_large.npy'))
+    except IOError:
+        print("Not found, generating...")
+        data = pd.read_pickle(os.path.join(DATA_DIR, 'large.pkl'))
+        data = data.sample(frac=1, replace=False, random_state = SEED)
 
-    split_index = int(0.8*len(data.index))
-    train_data = data[:split_index]['text']
-    y_train = np.array(data[:split_index]['rating'])
+        split_index = int(0.8*len(data.index))
+        train_data = data[:split_index]['text']
+        y_train = np.array(data[:split_index]['rating'])
 
-    test_data = data[split_index:]['text']
-    y_test = np.array(data[split_index:]['rating'])
+        test_data = data[split_index:]['text']
+        y_test = np.array(data[split_index:]['rating'])
 
-    tokenizer = pre_text.Tokenizer(NUM_WORDS)
-    tokenizer.fit_on_texts(train_data)
+        tokenizer = pre_text.Tokenizer(NUM_WORDS)
+        tokenizer.fit_on_texts(train_data)
 
-    train_sequences = tokenizer.texts_to_sequences(train_data)
-    test_sequences = tokenizer.texts_to_sequences(test_data)
+        train_sequences = tokenizer.texts_to_sequences(train_data)
+        test_sequences = tokenizer.texts_to_sequences(test_data)
 
-    x_train = sequence.pad_sequences(train_sequences, maxlen=SEQUENCE_LENGTH)
-    x_test = sequence.pad_sequences(test_sequences, maxlen=SEQUENCE_LENGTH)
+        x_train = sequence.pad_sequences(train_sequences, maxlen=SEQUENCE_LENGTH)
+        x_test = sequence.pad_sequences(test_sequences, maxlen=SEQUENCE_LENGTH)
 
-    np.save(join(DATA_DIR, 'x_train.npy'), x_train)
-    np.save(join(DATA_DIR, 'y_train.npy'), y_train)
-    np.save(join(DATA_DIR, 'x_test.npy'), x_test)
-    np.save(join(DATA_DIR, 'y_test.npy'), y_test)
+        np.save(join(DATA_DIR, 'x_train_large.npy'), x_train)
+        np.save(join(DATA_DIR, 'y_train_large.npy'), y_train)
+        np.save(join(DATA_DIR, 'x_test_large.npy'), x_test)
+        np.save(join(DATA_DIR, 'y_test_large.npy'), y_test)
+
+else:
+    try:
+        # load numpy matrices
+        print(f"Trying to load training data from {DATA_DIR}")
+
+        x_train = np.load(join(DATA_DIR, 'x_train.npy'))
+        y_train = np.load(join(DATA_DIR, 'y_train.npy'))
+        x_test = np.load(join(DATA_DIR, 'x_test.npy'))
+        y_test = np.load(join(DATA_DIR, 'y_test.npy'))
+    except IOError:
+        print("Not found, generating...")
+        data = pd.read_pickle(os.path.join(DATA_DIR, 'reduced.pkl'))
+        data = data.sample(frac=1, replace=False, random_state = SEED)
+
+        split_index = int(0.8*len(data.index))
+        train_data = data[:split_index]['text']
+        y_train = np.array(data[:split_index]['rating'])
+
+        test_data = data[split_index:]['text']
+        y_test = np.array(data[split_index:]['rating'])
+
+        tokenizer = pre_text.Tokenizer(NUM_WORDS)
+        tokenizer.fit_on_texts(train_data)
+
+        train_sequences = tokenizer.texts_to_sequences(train_data)
+        test_sequences = tokenizer.texts_to_sequences(test_data)
+
+        x_train = sequence.pad_sequences(train_sequences, maxlen=SEQUENCE_LENGTH)
+        x_test = sequence.pad_sequences(test_sequences, maxlen=SEQUENCE_LENGTH)
+
+        np.save(join(DATA_DIR, 'x_train.npy'), x_train)
+        np.save(join(DATA_DIR, 'y_train.npy'), y_train)
+        np.save(join(DATA_DIR, 'x_test.npy'), x_test)
+        np.save(join(DATA_DIR, 'y_test.npy'), y_test)
 
 print("DONE")
 
