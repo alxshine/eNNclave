8701e259674cbb3a159c7616a5006451aa9b59f9
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-04-14 14:07:23 +0200

    Changed flatten to GlobelAveragePool, 58.49%/55.23%

--- amazon_train.py
+++ amazon_train.py
@@ -145,7 +145,7 @@ model.add(layers.SeparableConv1D(filters=256, kernel_size=3, padding='same', act
 model.add(layers.MaxPooling1D(pool_size=2))
 model.add(layers.SeparableConv1D(filters=256, kernel_size=3, padding='same', activation='relu'))
 model.add(layers.MaxPooling1D(pool_size=2))
-model.add(layers.Flatten())
+model.add(layers.GlobalAveragePooling1D())
 
 model.add(layers.Dense(HIDDEN_NEURONS, activation='relu'))
 model.add(layers.Dropout(DROPOUT_RATE))
