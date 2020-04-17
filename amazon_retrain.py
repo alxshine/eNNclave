4bf1e0c90b0699758d9cc051fab6d13e83382b40
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-04-17 12:01:05 +0200

    Added new evaluation to retrain

--- amazon_retrain.py
+++ amazon_retrain.py
@@ -80,23 +80,7 @@ fig.plot(range(EPOCHS), history['val_mae'], label='Validation MAE')
 
 #  print(fig.show(legend=True))
 
-print("Generating true training accuracy")
-train_predictions = last_layer_model.predict(x_train, verbose = 0)
-train_cleaned_predictions = train_predictions.flatten().round()
-train_acc = np.mean(train_cleaned_predictions == y_train)
-train_mae = np.mean(np.abs(train_cleaned_predictions - y_train))
-
-print(f'True training accuracy: {train_acc*100:.4}')
-print(f'Training MAE: {train_mae:.4}')
-
-print("Generating true test accuracy")
-test_predictions = last_layer_model.predict(x_test, verbose = 0)
-test_cleaned_predictions = test_predictions.flatten().round()
-test_acc = np.mean(test_cleaned_predictions == y_test)
-test_mae = np.mean(np.abs(test_cleaned_predictions - y_test))
-
-print(f'True test accuracy: {test_acc*100:.4}')
-print(f'Test MAE: {test_mae:.4}')
+eval_true_accuracy(last_layer_model, x_test, y_test)
 
 # retrain dense layers
 print("\n\n####### Retraining dense layers #######")
@@ -141,23 +125,7 @@ fig.plot(range(EPOCHS), history['val_mae'], label='Validation MAE')
 
 #  print(fig.show(legend=True))
 
-print("Generating true training accuracy")
-train_predictions = dense_model.predict(x_train, verbose = 0)
-train_cleaned_predictions = train_predictions.flatten().round()
-train_acc = np.mean(train_cleaned_predictions == y_train)
-train_mae = np.mean(np.abs(train_cleaned_predictions - y_train))
-
-print(f'True training accuracy: {train_acc*100:.4}')
-print(f'Training MAE: {train_mae:.4}')
-
-print("Generating true test accuracy")
-test_predictions = dense_model.predict(x_test, verbose = 0)
-test_cleaned_predictions = test_predictions.flatten().round()
-test_acc = np.mean(test_cleaned_predictions == y_test)
-test_mae = np.mean(np.abs(test_cleaned_predictions - y_test))
-
-print(f'True test accuracy: {test_acc*100:.4}')
-print(f'Test MAE: {test_mae:.4}')
+eval_true_accuracy(dense_model, x_test, y_test)
 
 # retrain conv and dense layers
 print("\n\n####### Keeping only embedding and tokenizer #######")
@@ -202,23 +170,7 @@ fig.plot(range(EPOCHS), history['val_mae'], label='Validation MAE')
 
 #  print(fig.show(legend=True))
 
-print("Generating true training accuracy")
-train_predictions = conv_model.predict(x_train, verbose = 0)
-train_cleaned_predictions = train_predictions.flatten().round()
-train_acc = np.mean(train_cleaned_predictions == y_train)
-train_mae = np.mean(np.abs(train_cleaned_predictions - y_train))
-
-print(f'True training accuracy: {train_acc*100:.4}')
-print(f'Training MAE: {train_mae:.4}')
-
-print("Generating true test accuracy")
-test_predictions = conv_model.predict(x_test, verbose = 0)
-test_cleaned_predictions = test_predictions.flatten().round()
-test_acc = np.mean(test_cleaned_predictions == y_test)
-test_mae = np.mean(np.abs(test_cleaned_predictions - y_test))
-
-print(f'True test accuracy: {test_acc*100:.4}')
-print(f'Test MAE: {test_mae:.4}')
+eval_true_accuracy(conv_model, x_test, y_test)
 
 # retrain entire network
 print("\n\n####### Keeping only tokenizer #######")
@@ -258,20 +210,4 @@ fig.plot(range(EPOCHS), history['val_mae'], label='Validation MAE')
 
 #  print(fig.show(legend=True))
 
-print("Generating true training accuracy")
-train_predictions = full_model.predict(x_train, verbose = 0)
-train_cleaned_predictions = train_predictions.flatten().round()
-train_acc = np.mean(train_cleaned_predictions == y_train)
-train_mae = np.mean(np.abs(train_cleaned_predictions - y_train))
-
-print(f'True training accuracy: {train_acc*100:.4}')
-print(f'Training MAE: {train_mae:.4}')
-
-print("Generating true test accuracy")
-test_predictions = full_model.predict(x_test, verbose = 0)
-test_cleaned_predictions = test_predictions.flatten().round()
-test_acc = np.mean(test_cleaned_predictions == y_test)
-test_mae = np.mean(np.abs(test_cleaned_predictions - y_test))
-
-print(f'True test accuracy: {test_acc*100:.4}')
-print(f'Test MAE: {test_mae:.4}')
+eval_true_accuracy(full_model, x_test, y_test)
