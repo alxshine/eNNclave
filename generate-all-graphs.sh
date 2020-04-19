fe7f2f871eadaf63de8f875d1c56884f2d9ec98a
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-04-19 17:09:24 +0200

    Added amazon timing logs and tikz generation

--- generate-all-graphs.sh
+++ generate-all-graphs.sh
@@ -4,3 +4,4 @@ python tikz_generation.py --model models/mit.h5 --time timing_logs/mit_times_cpu
 python tikz_generation.py --model models/imdb.h5 --time timing_logs/imdb_times_cpu.csv timing_logs/imdb_times_gpu.csv --ymin -3 --ymax 1 > imdb.tex
 python tikz_generation.py --model models/mnist.h5 --time timing_logs/mnist_times_cpu.csv timing_logs/mnist_times_gpu.csv --ymin -4 --ymax 1 > mnist.tex
 python tikz_generation.py --model models/rotten_tomatoes.h5 --time timing_logs/rotten_times_cpu.csv timing_logs/rotten_times_gpu.csv --ymin -3 --ymax 1 > rotten.tex
+python tikz_generation.py --model models/amazon_dense.h5 --time timing_logs/amazon_times_cpu.csv --ymin -3 --ymax 1 > amazon.tex
