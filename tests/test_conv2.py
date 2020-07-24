70533da7cd2e66b4aeae616c737444c1bea57aec
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-07-24 11:59:15 +0200

    Add enclave tests to conv2

--- tests/test_conv2.py
+++ tests/test_conv2.py
@@ -2,6 +2,7 @@ from tensorflow.python.framework.test_util import TensorFlowTestCase
 from tensorflow.keras.models import load_model, Sequential
 import tensorflow.keras.layers as layers
 import numpy as np
+import os
 
 import unittest
 from .common import common_test_basis
@@ -28,11 +29,41 @@ class Conv2dTests(TensorFlowTestCase):
 
     def testHuge(self):
         raise AssertionError("Causes a segfault in C")
-
         model = Sequential([
             layers.Conv2D(64, 5, input_shape=(500, 500, 64), padding='same')
         ])
         common_test_basis(model, False)
+
+    @unittest.skipIf(os.environ.get('SGX_SDK') is None, "SGX is not available")
+    def testSmallEnclave(self):
+        model = Sequential([
+            layers.Conv2D(3, 3, input_shape=(5, 5, 3), padding='same')
+        ])
+        common_test_basis(model, True)
+
+    @unittest.skipIf(os.environ.get('SGX_SDK') is None, "SGX is not available")
+    def testMediumEnclave(self):
+        model = Sequential([
+            layers.Conv2D(5, 3, input_shape=(10, 10, 5), padding='same')
+        ])
+        common_test_basis(model, True)
+
+    @unittest.skipIf(os.environ.get('SGX_SDK') is None, "SGX is not available")
+    def testLargeEnclave(self):
+        model = Sequential([
+            layers.Conv2D(10, 4, input_shape=(100, 100, 5), padding='same')
+        ])
+        common_test_basis(model, True)
+
+    
+    @unittest.skipIf(os.environ.get('SGX_SDK') is None, "SGX is not available")
+    def testHugeEnclave(self):
+        raise AssertionError("Causes a segfault in C")
+
+        model = Sequential([
+            layers.Conv2D(64, 5, input_shape=(500, 500, 64), padding='same')
+        ])
+        common_test_basis(model, True)
 
 
 if __name__ == "__main__":
