822f216b1cd4c07e12f7e2ffb757e7829f9191ee
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-07-28 14:21:10 +0200

    clean up matutil definitions

--- inc/nn.h
+++ inc/nn.h
@@ -8,39 +8,34 @@
 extern "C" {
 #endif
 
-    int matutil_initialize();
+int matutil_multiply(float* m1, int h1, int w1, float* m2, int h2, int w2, float* ret);
 
-    int matutil_teardown();
+int
+matutil_add(float* m1, int h1, int w1, float* m2, int h2, int w2, float* ret); // TODO: this doesn't need 2 dimensions
 
-    void matutil_get_new_dimensions(int h1, int w1, int h2, int w2, int *rr, int *cr);
+int matutil_sep_conv1(float* input, int steps, int c, int f, float* depth_kernels, float* point_kernels, int ks,
+                      float* biases, float* ret);
 
-    int matutil_multiply(float *m1, int h1, int w1, float *m2, int h2, int w2, float *ret);
+int matutil_conv2(float* input, int h, int w, int c, int f, float* kernels, int kh, int kw, float* biases, float* ret);
 
-    int matutil_add(float *m1, int h1, int w1, float *m2, int h2, int w2, float *ret); // TODO: this doesn't need 2 dimensions
+int matutil_depthwise_conv2(float* input, int h, int w, int c, int padding, float* kernels, int kh, int kw, float* ret);
 
-    int matutil_sep_conv1(float *input, int steps, int c, int f, float *depth_kernels, float *point_kernels, int ks, float *biases, float *ret);
+void matutil_relu(float* m, int h, int w); // TODO: This doesn't need 2 dimensions
 
-    int matutil_conv2(float *input, int h, int w, int c, int f, float *kernels, int kh, int kw, float *biases, float *ret);
+void matutil_global_average_pooling_1d(float* m, int steps, int c, float* ret);
 
-    int matutil_depthwise_conv2(float *input, int h, int w, int c, int padding, float *kernels, int kh, int kw, float *ret);
+void matutil_global_average_pooling_2d(float* m, int h, int w, int c, float* ret);
 
-    void matutil_relu(float *m, int h, int w); // TODO: This doesn't need 2 dimensions
+void matutil_max_pooling_1d(float* m, int steps, int c, int pool_size, float* ret);
 
-    void matutil_global_average_pooling_1d(float *m, int steps, int c, float *ret);
+void matutil_max_pooling_2d(float* m, int h, int w, int c, int pool_size, float* ret);
 
-    void matutil_global_average_pooling_2d(float *m, int h, int w, int c, float *ret);
+void
+matutil_zero_pad2(float* m, int h, int w, int c, int top_pad, int bottom_pad, int left_pad, int right_pad, float* ret);
 
-    void matutil_max_pooling_1d(float *m, int steps, int c, int pool_size, float *ret);
+void matutil_dump_matrix(float* m, int r, int c);
 
-    void matutil_max_pooling_2d(float *m, int h, int w, int c, int pool_size, float *ret);
-
-    void matutil_zero_pad2(float *m, int h, int w, int c, int top_pad, int bottom_pad, int left_pad, int right_pad, float *ret);
-
-    int matutil_forward(float *m, int size, int *label);
-
-    void matutil_dump_matrix(float *m, int r, int c);
-
-    void matutil_dump_matrix3(float *m, int h, int w, int c);
+void matutil_dump_matrix3(float* m, int h, int w, int c);
 
 #if defined(__cplusplus)
 }
