0b0da4172bb6603bc18658864fafe63f74f9fbe7
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-08-11 12:59:20 +0200

    Fix include in setup.py, add author and version

--- frontend/python/setup.py
+++ frontend/python/setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup, Extension
+from os.path import join
+import os
 
 setup(name='eNNclave',
+      version='1.0',
+      author="Alexander Schlögl",
+      description="Python frontend for eNNclave",
+      author_email="alexander.schloegl@uibk.ac.at",
       py_modules=['ennclave'],
       ext_modules=[Extension('frontend_python', ['interoperability/frontend_python.c'], libraries=['dl'],
-                             include_dirs=['../../inc'])],
-      install_requires=['jinja2', 'tensorflow==2.2.0', 'numpy', 'invoke']
+                             include_dirs=[join(os.getenv('ENNCLAVE_HOME'), 'inc')])],
+      install_requires=['jinja2', 'tensorflow-gpu', 'numpy', 'invoke']
       )
