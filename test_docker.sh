30822b4eabd52dfd9cfe7c4099922b325002df60
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-08-11 13:55:52 +0200

    Add encryptor build for tests

--- test_docker.sh
+++ test_docker.sh
@@ -9,4 +9,5 @@ export LD_LIBRARY_PATH=/eNNclave/lib:$LD_LIBRARY_PATH
 #(cd backend/native/tests && python generate_tests.py)
 (cd build && cmake ..)
 #(cd build && make -j8 core_tests && ./core_tests)
+(cd build && make backend_sgx_encryptor)
 (cd frontend/python &&  pip install . && python -m unittest discover -f)
