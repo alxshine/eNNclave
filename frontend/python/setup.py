4e9487d47aa2e879259f2efc77fe619997fb1de3
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-09-09 17:26:11 +0200

    Changed setup name of ennclave frontend

--- frontend/python/setup.py
+++ frontend/python/setup.py
@@ -8,7 +8,7 @@ setup(name='eNNclave',
       description="Python frontend for eNNclave",
       author_email="alexander.schloegl@uibk.ac.at",
       py_modules=['ennclave'],
-      ext_modules=[Extension('frontend_python', ['interoperability/frontend_python.c'], libraries=['dl'],
+      ext_modules=[Extension('ennclave_inference', ['interoperability/frontend_python.c'], libraries=['dl'],
                              include_dirs=[join(os.getenv('ENNCLAVE_HOME'), 'inc')])],
       install_requires=['jinja2', 'tensorflow-gpu', 'numpy', 'invoke']
       )
