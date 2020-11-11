8a4bc56a03e015fc33658161a6999380ee1174ef
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-11-11 17:15:54 +0000

    Remove unnecessary python scripts

--- generation_test2.py
+++
@@ -1,8 +0,0 @@
-import lib.frontend_python as frontend_python
-import numpy as np
-
-if __name__ == '__main__':
-    inputs = np.arange(5, dtype=np.float32)
-    result_bytes = frontend_python.native_forward(inputs.tobytes(), 5, 5)
-    result = np.frombuffer(result_bytes, dtype=np.float32)
-    print(result)
